abbe2bd92f036b1f58d442d297230f3d666c6f6f
Dipendra-Paudel <dipenbro006@gmail.com>
2022-06-25 15:42:52 +0545

    404 page updated

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import Navigation from "./static/Navigation";
+import TopBar from "./static/TopBar";
+import Footer from "./static/Footer";
 
 const NotFound = () => {
   return (
-    <div className="py-20 px-4 md:px-8 lg:px-12 xl:px-20 text-center bg-gray-100">
-      <div>
-        <div className="text-5xl lg:text-7xl">404</div>
-        <div className="text-gray-700">
-          The page you are looking is not found on the server
+    <div>
+      <TopBar />
+      <Navigation />
+
+      <div className="py-20 px-4 md:px-8 lg:px-12 xl:px-20 text-center bg-gray-100">
+        <div>
+          <div className="text-5xl lg:text-7xl">404</div>
+          <div className="text-gray-700">
+            The page you are looking is not found on the server
+          </div>
         </div>
       </div>
+
+      <Footer />
     </div>
   );
 };
